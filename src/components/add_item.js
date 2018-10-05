0b31a572c2b93b95987b2b9ff947b695f817bfc0
Rachel Pan <rpan06@gmail.com>
2018-10-05 11:55:34 -0700

    added item using redux form

--- src/components/add_item.js
+++ src/components/add_item.js
@@ -1,21 +1,26 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form'
+import { connect } from 'react-redux';
+import {addListItem} from '../actions'
 
 class AddItem extends Component {
-    renderInput(props){
-        console.log('render input arguments', props)
+    renderInput({ input, label, meta: {touched, error}}){ //able to destructure in parameters
+        // console.log('render input arguments', props)
+        // const { input, label, meta: {touched, error}} = props;
         return(
             <div className="row">
                 <div className="s12">
-                    <label>Label Here</label>
-                    <input {...props.input} type="text"/>
+                    <label>{label}</label>
+                    <input {...input} type="text"/>
+                    <p className="red-text text-darken-2">{touched && error}</p>
                 </div>
             </div>
         )
     }
-    saveItem=(values)=>{
-        console.log("Form Values: ",values)
-        //do stuff here;
+    saveItem = async (values)=>{
+        console.log("Form Values: ", values)
+        await this.props.addListItem(values);
+        this.props.history.push('/');
     }
     render(){
         const {handleSubmit} = this.props
@@ -25,8 +30,8 @@ class AddItem extends Component {
                 <div className="row">
                     <div className="col s8 offset-s2">
                         <form onSubmit={handleSubmit(this.saveItem)}>
-                            <Field name="title" component={this.renderInput}/>
-                            <Field name="details" component={this.renderInput}/>
+                            <Field name="title" component={this.renderInput} label="Title"/>
+                            <Field name="details" component={this.renderInput} label="Details"/>
                             <div className="row">
                                 <div className="s12 right-align">
                                     <button className="btn blue-grey">Add Item</button>
@@ -40,6 +45,23 @@ class AddItem extends Component {
     }
 }
 
-export default reduxForm({
-    form: 'add-item' //no specific name, just made up
+function validateForm(values){
+    const {title, details} = values;
+    const errors = {};
+    if (!title){
+        errors.title = 'Please give your item a title'
+    }
+    if(!details){
+        errors.details = 'Please give your item details'
+    }
+    return errors
+}
+
+AddItem = reduxForm({
+    form: 'add-item', //no specific name, just made up
+    validate: validateForm //keys matter, but values don't. made up values
 })(AddItem);
+
+export default connect(null, {
+    addListItem
+})(AddItem)
